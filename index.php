b860ec69bdc4c19379e0d4993c1cae4a9a67a887
octopus6663252 <78371672+octopus6663252@users.noreply.github.com>
2021-02-01 19:35:15 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,173 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="">
+    <title class="lol">Битва за Москву</title>
+  </head>
+  <body>
+  <?php require "blocs\header.php" ?>
+  <section class="jumbotron text-center rounded-0 bg-white">
+    <div class="container">
+      <h1 class="jumbotron-heading">«БИТВА ЗА МОСКВУ»</h1>
+      <p class="lead text-muted">Проект "Битва за Москву", выполненный 9 "Б" классом МБОУ СОШ 99. Рассказывает о подвигах и событиях сражения за Москву во время Великой Отечественной Войны.</p>
+    </div>
+  </section>
+  <main role="main">
+  <div class="bg-light py-5">
+  <div class="container">
+
+      <div class="row">
+        <div class="col-md-4">
+          <div class="card mb-4 shadow border-0">
+          <img src="https://cdn.discordapp.com/attachments/774205603911696404/805813063193854002/300px-RIAN_archive_887721_Defense_of_Moscow.png" class="card-img-top" height="225"  alt="...">
+            <div class="card-body">
+            <h5 class="card-title">Начало военных действий</h5>
+              <p class="card-text">Война с Советским Союзом, несмотря на первоначальные победы, развивалась для немецкого командования несколько по иному сценарию нежели с Польшей или западноевропейскими странами.</p>
+              <div class="d-flex justify-content-between align-items-center">
+                  <button type="button" class="btn btn-sm btn-outline-secondary" data-toggle="modal" data-target="#exampleModal">Подробнее</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- Тело модального окна -->
+        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Начало военных действий</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+      <img src="https://cdn.discordapp.com/attachments/774205603911696404/805813063193854002/300px-RIAN_archive_887721_Defense_of_Moscow.png" class="card-img-top rounded" height="225"  alt="...">
+      Война с Советским Союзом, несмотря на первоначальные победы, развивалась для немецкого командования несколько по иному сценарию нежели с Польшей или западноевропейскими странами.
+      План блицкрига (Операция «Барбаросса») предполагал взятие Москвы в течение первых 10-12 недель войны. Однако, несмотря на успехи вермахта в первые дни наступления, 
+      усилившееся сопротивление Красной армии и ряд объективных причин помешали его выполнению.
+
+      Только Смоленское сражение июля — сентября 1941 года задержало продвижение захватчиков к Москве на два месяца. Германские стратеги не смогли в полной мере предусмотреть всех издержек, 
+      связанных со значительным расширением фронта, износом материальной части ударных группировок и падением наступательного духа солдат и офицеров в случае непредвиденного упорного 
+      сопротивления противника.
+
+      К началу сентября 1941 года московское направление оставалось главным для верховного немецкого командования. В телеграмме ОКХ от 31 июля 1941 года группе армий «Центр» 
+      было приказано по-прежнему «готовиться к наступлению на Москву».
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Закрыть</button>
+      </div>
+    </div>
+  </div>
+      </div>
+      <!-- Конец тела модального окна -->
+        <div class="col-md-4">
+          <div class="card mb-4 shadow border-0">
+          <img src="https://cdn.discordapp.com/attachments/774205603911696404/805814238634836008/58f8a029c36188722f8b45eb.png" class="card-img-top" height="225"  alt="...">
+            <div class="card-body">
+            <h5 class="card-title">Фашисты у стен Москвы</h5>
+              <p class="card-text">В оборонительный период битвы Красная армия отстояла Москву. Наступление противника, несмотря на успехи лета — осени 1941 года, было остановлено.</p>
+              <div class="d-flex justify-content-between align-items-center">
+                  <button type="button" class="btn btn-sm btn-outline-secondary" data-toggle="modal" data-target="#exampleModal_2">Подробнее</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- Тело модального окна -->
+        <div class="modal fade" id="exampleModal_2" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Фашисты у стен Москвы</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+      <img src="https://cdn.discordapp.com/attachments/774205603911696404/805814238634836008/58f8a029c36188722f8b45eb.png" class="card-img-top rounded" height="225"  alt="...">
+      В оборонительный период битвы Красная армия отстояла Москву. Наступление противника, несмотря на успехи лета — осени 1941 года, было остановлено.
+      Враг не сумел достичь своей главной цели блицкрига — уничтожить советские вооруженные силы до наступления зимы. Германия оказалась перед неизбежностью затяжной войны с СССР.
+      С конца октября по начало декабря 1941 года удалось укрепить фронт и подтянуть из глубины страны свежие дивизии и вооружение с техникой. 
+      В состав только Западного фронта были переданы три общевойсковые армии (1-я ударная, 20-я и 10-я), девять стрелковых и две кавалерийские дивизии, восемь стрелковых, 
+      шесть танковых бригад и большое количество специальных частей.
+      Калининский фронт и правое крыло Юго-Западного фронта также были заметно пополнены людьми и вооружением. Авиационные соединения этих фронтов были пополнены частями 
+      Московского военного округа, 6-го истребительного корпуса ПВО и дальней бомбардировочной авиации из резерва Главного Командования.
+      К началу декабрьского контрнаступления насчитывалось уже более 1,1 млн человек, 7652 орудий и миномётов, 415 установок реактивной артиллерии, 774 танка (в том числе 222 тяжёлых и средних)
+      и 1 тыс. самолётов. В немецкой группе армий «Центр» было 1 708 тыс. человек, около 13 500 орудий и миномётов, 1170 танков и 615 самолётов.
+      С целью предотвращения дальнейшего усиления группы армий «Центр» за счёт войск групп армий «Север» и «Юг», советскими войсками были проведены наступательные операции: 
+      Тихвинская (с 10 ноября) и Ростовская (с 17 ноября).
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Закрыть</button>
+      </div>
+    </div>
+  </div>
+      </div>
+      <!-- Конец тела модального окна -->
+        <div class="col-md-4">
+          <div class="card mb-4 shadow border-0">
+          <img src="https://cdn.discordapp.com/attachments/774205603911696404/805813155577200670/parad-7-noyabrya-1941-goda.png" class="card-img-top" height="225"  alt="...">
+            <div class="card-body">
+            <h5 class="card-title">Итоги сражений</h5>
+              <p class="card-text">В ходе сражения немецкие войска потерпели ощутимое поражение. В результате контрнаступления и общего наступления они были отброшены на 100—250 км.</p>
+              <div class="d-flex justify-content-between align-items-center">
+                  <button type="button" class="btn btn-sm btn-outline-secondary" data-toggle="modal" data-target="#exampleModal_3">Подробнее</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Тело модального окна -->
+      <div class="modal fade" id="exampleModal_3" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Итоги сражений</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+      <img src="https://cdn.discordapp.com/attachments/774205603911696404/805813155577200670/parad-7-noyabrya-1941-goda.png" class="card-img-top rounded" height="225"  alt="...">
+      В ходе сражения немецкие войска потерпели ощутимое поражение. В результате контрнаступления и общего наступления они были отброшены на 100—250 км. Полностью были освобождены Тульская, 
+      Рязанская и Московская области, многие районы Калининской, Смоленской и Орловской областей.
+      В то же время силы вермахта смогли сохранить фронт и Ржевско-Вяземский плацдарм. Советским войскам не удалось разгромить группу армий «Центр». 
+      Таким образом, решение вопроса об обладании стратегической инициативой было отложено до летней кампании 1942 года.
+      Вопреки распространённой сегодня точке зрения, многие немецкие генералы высоко оценивали боевые качества красноармейцев. Через месяц боёв Гальдер записывает окончательный 
+      и крайне неприятный для германского командования вывод, сделанный фельдмаршалом Браухичем: «Своеобразие страны и своеобразие характера русских придаёт кампании особую специфику. 
+      Первый серьёзный противник».
+      К тому же выводу приходит и командование группы армий «Юг»: «Силы, которые нам противостоят, являются по большей части решительной массой, 
+      которая в упорстве ведения войны представляет собой нечто совершенно новое по сравнению с нашими бывшими противниками. Мы вынуждены признать, 
+      что Красная Армия является очень серьёзным противником… Русская пехота проявила неслыханное упорство прежде всего в обороне стационарных укреплённых сооружений. 
+      Даже в случае падения всех соседних сооружений некоторые ДОТы, призываемые сдаться, держались до последнего человека».
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Закрыть</button>
+      </div>
+    </div>
+  </div>
+      </div>
+      <!-- Конец тела модального окна -->
+
+</main>
+        <?php require "blocs\ooter.php" ?>
+
+    <script src="js/anime.min.js"></script>
+    <script>
+    </script>
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+    <!-- Option 2: jQuery, Popper.js, and Bootstrap JS
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
