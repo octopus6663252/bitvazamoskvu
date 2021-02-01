4013f5c5c722440d9d0e3854e273fd909f59db94
octopus6663252 <78371672+octopus6663252@users.noreply.github.com>
2021-02-01 19:34:39 +0300

    Add files via upload

---
+++ blocs/ooter.php
@@ -0,0 +1,9 @@
+<footer class="text-white pt-4 pb-1 bg-dark">
+  <div class="container">
+    <p class="float-right">
+      <a href="#" class="text-white">Back to top</a>
+    </p>
+    <p>По заказу Котляровы Юлии Борисовны</p>
+    <p>Originally made by © Artem Zhigulin and © Egor Antipenko</p>
+  </div>
+</footer>
